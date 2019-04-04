fc412be73f43225f0abae6ae5aabd6e93781f535
lstasio5 <lisa.stasio@gmail.com>
2019-04-04 19:38:01 -0400

    updated js to pull questions from array and display on html using jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,65 @@
-//Psuedo Code
+// My trivia questions
+var questions = [
+  {
+    question: "The hawaiian Islands used to be known as the Cook Islands",
+    answers: ["True", "False"],
+    correctAnswer: "True"
+  },
+  {
+    question: "Kauai is the largest of the Hawaiian islands",
+    answers: ["True", "False"],
+    correctAnswer: "False"
+  },
+  {
+    question: "Hawaii is known for it's great skiing",
+    answers: ["True", "False"],
+    correctAnswer: "False"
+  },
+];
 
-//When user selects Start display a one minute countdown timer on the top of the screen
+//function to start timer and show questions
 
-//Run the time until all questions have been answered
+$(document).ready(function(){
+  $("button").click(function(){
+    onTimer();
 
-window.onload= function(){
-    onTimer()
+    $("#start").remove();
+
+    // need to convert to loop
+    $("#card").html(`
+      <h2>${questions[0].question}</h2>
+      <input type='radio' name='question1' value=${questions[0].answers[0]}>true
+      <input type='radio' name='question1' value=${questions[0].answers[1]}>false
+      <h2>${questions[1].question}</h2>
+      <input type='radio' name='question2' value=${questions[0].answers[0]}>true
+      <input type='radio' name='question2' value=${questions[0].answers[1]}>false
+      <h2>${questions[2].question}</h2>
+      <input type='radio' name='question3' value=${questions[0].answers[0]}>true
+      <input type='radio' name='question3' value=${questions[0].answers[1]}>false
+    `)
+
+  });
+});
+
+ 
+var i = 60;
+console.log(onTimer)
+
+function onTimer() {
+  document.getElementById('my-counter').innerHTML = i;
+  i--;
+  if (i < 0) {
+    alert('Sorry, time is up!');
+  } else {
+      setTimeout(onTimer, 1000);
     }
-    i = 60;
-    console.log(onTimer)
-    
-    function onTimer() {
-      document.getElementById('mycounter').innerHTML = i;
-      i--;
-      if (i < 0) {
-        alert('You lose!');
-      } else {
-        setTimeout(onTimer, 1000);
-      }
-    }
+  }
+
+  //Pseudo code
+
+  // If all answers for each question is correct and time is greater than 0 
+  // then user wins
+
+  // If all answers for each question are not correct and time is
+  //less than zero than user loses
+
